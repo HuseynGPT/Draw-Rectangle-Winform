35ffe86084fe85443643d708eccb0b3bf9febd85
Huseyn Huseynov <131697602+HuseynGPT@users.noreply.github.com>
2023-10-06 20:39:49 +0400

    Update Form1.cs

--- Paint Task/Form1.cs
+++ Paint Task/Form1.cs
@@ -9,6 +9,18 @@ namespace Paint_Task
             InitializeComponent();
         }
 
+        private void Rectangle_Clicked(object sender, MouseEventArgs e)
+        {
+            Label lbl = sender as Label;
+            this.Text = lbl.Location.ToString();
+        }
+
+        private void Rectangle_DoubleClicked(object sender, MouseEventArgs e)
+        {
+            Label lbl = sender as Label;
+            Controls.Remove(lbl);
+        }
+
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             start = new(e.X, e.Y);
@@ -24,13 +36,25 @@ namespace Paint_Task
             else
             {
                 
-               
+               // Bu hissede komeklik almisam
                 Label rectangle = new();
                 rectangle.Size = new(width, heigth);
-                rectangle.BackColor = Color.Black;
-                rectangle.ForeColor = Color.Black;
-                rectangle.Location = new(width, heigth);
+                rectangle.BackColor = Color.Green;
+                rectangle.ForeColor = Color.Green;
+                int x = start.X - end.X;
+                int y = start.Y - end.Y;
+                if (x > 0 && y < 0)
+                { rectangle.Location = new Point(end.X, start.Y); }
+                else if (x < 0 && y > 0)
+                { rectangle.Location = new Point(start.X, end.Y); }
+                else if (x > 0 && y > 0)
+                { rectangle.Location = end; }
+                else if (x < 0 && y < 0)
+                { rectangle.Location = start; }
+                rectangle.MouseClick += Rectangle_Clicked;
+                rectangle.MouseDoubleClick += Rectangle_DoubleClicked;
                 Controls.Add(rectangle);
+
 
             }
         }
